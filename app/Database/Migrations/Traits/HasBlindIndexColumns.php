1b5df8faba1f5d3336ff27040a175e34257e28f0
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-05-26 22:34:01 +0200

    Add trait for blind index columns

---
+++ app/Database/Migrations/Traits/HasBlindIndexColumns.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations\Traits;
+
+use Illuminate\Database\Schema\Blueprint;
+
+trait HasBlindIndexColumns
+{
+    protected function addBlindIndexColumns(Blueprint $table, array $fields): void
+    {
+        foreach ($fields as $field => $options) {
+            $unique = false;
+            $nullable = false;
+
+            if (is_bool($options)) {
+                $unique = $options;
+            } elseif (is_array($options)) {
+                $unique = $options['unique'] ?? false;
+                $nullable = $options['nullable'] ?? false;
+            }
+
+            $textColumn = $table->text($field);
+            if ($nullable) {
+                $textColumn->nullable();
+            }
+
+            $indexColumn = $table->char($field . '_blind_index', 64);
+            if ($nullable) {
+                $indexColumn->nullable();
+            }
+
+            if ($unique) {
+                $indexColumn->unique();
+            } else {
+                $indexColumn->index();
+            }
+        }
+    }
+}
