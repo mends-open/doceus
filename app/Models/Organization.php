98ae16605733ff6e6dfbf18d22977fb25ac53d2f
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-05-27 18:54:08 +0200

    Add soft delete support for core models and tests

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -2,8 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Organization extends Model
 {
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $guarded = [];
 }
--- app/Models/Personnel.php
+++ app/Models/Personnel.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Personnel extends Model
 {
+    use HasFactory, HasUuids, SoftDeletes;
+
     protected $table = 'personnel';
+    protected $guarded = [];
 }
