0d031213a901adb1a90973f1500d7b1b9e78f47a
dominik-mukrecki <dominik@mukrecki.pl>
2025-05-27 18:59:07 +0200

    Add new Filament resources for Organization, Unit, and Personnel
    This commit introduces resources for managing Organizations, Units, and Personnel, including listing, creating, and editing functionality. These resources are structured using Filament's resource and page system to simplify CRUD operations.

---
+++ app/Filament/Resources/PersonnelResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PersonnelResource\Pages;
+use App\Filament\Resources\PersonnelResource\RelationManagers;
+use App\Models\Personnel;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PersonnelResource extends Resource
+{
+    protected static ?string $model = Personnel::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPersonnels::route('/'),
+            'create' => Pages\CreatePersonnel::route('/create'),
+            'edit' => Pages\EditPersonnel::route('/{record}/edit'),
+        ];
+    }
+}
