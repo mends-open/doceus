dbd1a8f4859a552a2f4ec76083f5822ec37df983
dominik-mukrecki <dominik@mukrecki.pl>
2025-05-27 18:50:08 +0200

    Add models and migrations for organizations, units, and personnel
    Introduce database structure for organizations, units, and personnel tables with relationships and timestamps. Include corresponding Eloquent models for each, establishing the foundational structure for these entities.

---
+++ database/migrations/2025_05_27_163745_create_personnel_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personnel', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('unit_id')->constrained('units', 'id');
+            $table->foreignUuid('user_id')->constrained('users', 'id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personnel');
+    }
+};
