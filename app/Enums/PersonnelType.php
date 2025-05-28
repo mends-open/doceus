727a78736fad038ed45ff90c0e1888c6eec7e815
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-05-28 21:44:22 +0200

    refactor default entities creation

--- app/Enums/PersonnelType.php
+++ app/Enums/PersonnelType.php
@@ -5,14 +5,12 @@ namespace App\Enums;
 enum PersonnelType: string
 {
     case MEDICAL_DOCTOR = 'medical_doctor';
-    case DENTISTRY_DOCTOR = 'dentistry_doctor';
     case MEDICAL_ASSISTANT = 'medical_assistant';
 
     public function label(): string
     {
         return match ($this) {
             self::MEDICAL_DOCTOR => __('doceus.personnel.medical_doctor'),
-            self::DENTISTRY_DOCTOR => __('doceus.personnel.dentistry_doctor'),
             self::MEDICAL_ASSISTANT => __('doceus.personnel.medical_assistant'),
         };
     }
