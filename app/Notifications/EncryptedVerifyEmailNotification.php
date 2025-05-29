b78b2beacc77e926a36e712f7006a35da2402924
dominik-mukrecki <dominik@mukrecki.pl>
2025-05-30 01:27:55 +0200

    Remove unused tests and notifications
    Deleted obsolete tests for features and roles, as well as encrypted email and password notifications. These items were no longer relevant to the current codebase and removing them simplifies maintenance.

--- app/Notifications/EncryptedVerifyEmailNotification.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Notifications;
-
-use Filament\Notifications\Auth\VerifyEmail;
-use Illuminate\Contracts\Queue\ShouldBeEncrypted;
-
-class EncryptedVerifyEmailNotification extends VerifyEmail implements ShouldBeEncrypted {}
