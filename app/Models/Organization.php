eeba4163cfa87b4146ad92cf86ffe41150da0e9f
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-05-30 15:09:28 +0200

    Rename RoleType enum to UserFeature and update references

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -28,7 +28,7 @@ class Organization extends Model
     {
         return $this->belongsToMany(User::class)
             ->using(OrganizationUser::class)
-            ->withPivot('role_type');
+            ->withPivot('feature');
     }
 
 }
