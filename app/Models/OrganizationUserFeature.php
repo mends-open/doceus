e0b35ce55cdcb5ad8d7088e33001d9f4f4ec9165
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-05-30 19:01:01 +0200

    Remove unused Pivot import

--- app/Models/OrganizationUserFeature.php
+++ app/Models/OrganizationUserFeature.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class OrganizationUserFeature extends Model
 {
