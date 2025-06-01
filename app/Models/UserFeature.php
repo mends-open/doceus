342c97b2e227f14af5996438802d8a0869759a24
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-01 15:38:12 +0200

    Add `laravel-ide-helper` to development dependencies and enhance Eloquent model docblocks
    Included `barryvdh/laravel-ide-helper` in `require-dev` for improved IDE support. Updated multiple Eloquent models with detailed PHPDoc annotations to enhance auto-completion and type hinting.

--- app/Models/UserFeature.php
+++ app/Models/UserFeature.php
@@ -4,6 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+/**
+ * 
+ *
+ * @property string|null $organization_id
+ * @property string|null $user_id
+ * @property string|null $feature
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeature newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeature newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeature query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeature whereFeature($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeature whereOrganizationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeature whereUserId($value)
+ * @mixin \Eloquent
+ */
 class UserFeature extends Pivot
 {
     //
--- app/Models/UserFeatureEvent.php
+++ app/Models/UserFeatureEvent.php
@@ -10,6 +10,33 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * 
+ *
+ * @property string $id
+ * @property string $organization_id
+ * @property string $user_id
+ * @property UserFeature $feature
+ * @property FeatureEvent $event
+ * @property string $created_at
+ * @property string $created_by
+ * @property-read \App\Models\User $creator
+ * @property-read \App\Models\OrganizationUser|null $pivot
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Organization> $organizations
+ * @property-read int|null $organizations_count
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent whereEvent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent whereFeature($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent whereOrganizationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|UserFeatureEvent whereUserId($value)
+ * @mixin \Eloquent
+ */
 class UserFeatureEvent extends Model
 {
     use HasFactory, HasUuids;
