b9ec35200d4f13626962a69d6c90c29e676ac62f
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-04 13:56:07 +0200

    Refactor and migrate database structure for consistency and clarity
    Simplified migration structure by normalizing table names, refactoring enums, and incorporating streamlined PostgreSQL-specific macros. Removed legacy schema files and replaced them with the updated migrations for improved maintainability and consistency.

---
+++ database/migrations/2025_06_03_203058_create_organizations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\OrganizationType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', Arr::pluck(OrganizationType::cases(), 'value'));
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organizations');
+    }
+};
