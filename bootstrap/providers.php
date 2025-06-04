d78a2b0517eb0f1bb919fad829ebe735d88ba9b8
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-04 13:59:13 +0200

    Add `sqids/sqids` dependency and remove unused `MacrosServiceProvider`

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
-    App\Providers\MacrosServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
