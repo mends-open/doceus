cc84a854504bbfb6ec0a403216e453bb5ab6b576
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-04 13:56:07 +0200

    Refactor and migrate database structure for consistency and clarity
    Simplified migration structure by normalizing table names, refactoring enums, and incorporating streamlined PostgreSQL-specific macros. Removed legacy schema files and replaced them with the updated migrations for improved maintainability and consistency.

--- database/migrations/2025_06_03_203031_create_users_table.php
+++ database/migrations/2025_06_03_203031_create_users_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Enums\Language;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,31 +14,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-
-            $table->blind('email')->unique();
+            $table->id();
             $table->text('email');
-            $table->blind('first_name')->nullable();
             $table->text('first_name')->nullable();
-            $table->blind('last_name')->nullable();
             $table->text('last_name')->nullable();
-            $table->blind('pesel')->unique()->nullable();
             $table->text('pesel')->nullable();
-
-            $table->enum('language', ['en', 'pl'])->nullable();
-
+            $table->enum('language', Arr::pluck(Language::cases(), 'value'))->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email'); // unencrypted
+            $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
-
     }
 
     /**
@@ -45,6 +40,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
     }
 };
--- database/migrations/2025_06_04_115244_create_audits_table.php
+++ database/migrations/2025_06_04_115244_create_audits_table.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Enums\OrganizationType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('organizations', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->enum('type', Arr::pluck(OrganizationType::cases(), 'value'));
+        Schema::create('audits', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('organizations');
+        Schema::dropIfExists('audits');
     }
 };
