16ac41fdb95359168d60af4082517a12a8bd3ca2
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-08 20:40:19 +0200

    Add revision feature tests and person factory

---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Organization;
+use App\Models\Person;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PersonFactory extends Factory
+{
+    protected $model = Person::class;
+
+    public function definition(): array
+    {
+        return [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'pesel' => $this->faker->numerify('#############'),
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'organization_id' => Organization::factory(),
+        ];
+    }
+}
