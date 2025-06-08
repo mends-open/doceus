394d1a6a3a399f7200374ac0fa77802430ebfc8d
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-09 01:49:58 +0200

    Refactor relations in `Person`, `Phone`, and `Email` models: replace `HasMany` with `BelongsToMany` and integrate pivot models.

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -5,33 +5,12 @@ namespace App\Models;
 use App\Feature\Revision\Interfaces\Revisionable;
 use App\Feature\Revision\Observers\RevisionableObserver;
 use App\Feature\Revision\Traits\LogsRevisions;
+use App\Feature\Sqid\Interfaces\Sqidable;
+use App\Feature\Sqid\Traits\HasSqids;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use App\Models\Person;
-use App\Models\EmailPerson;
-    public function persons(): BelongsToMany
-        return $this->belongsToMany(Person::class)->using(EmailPerson::class);
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
-/**
- *
- *
- * @property int $id
- * @property int $person_id
- * @property mixed $email
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Models\Person $person
- * @method static \Database\Factories\EmailFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email query()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email wherePersonId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Email whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 
 #[ObservedBy([RevisionableObserver::class])]
 class Email extends Model implements Revisionable, Sqidable
@@ -39,7 +18,10 @@ class Email extends Model implements Revisionable, Sqidable
     use HasFactory, HasSqids, LogsRevisions;
 
     protected $fillable = [
-        'person_id',
+        'email',
+    ];
+
+    protected array $revisionable = [
         'email',
     ];
 
@@ -47,8 +29,8 @@ class Email extends Model implements Revisionable, Sqidable
         'email' => 'encrypted',
     ];
 
-    public function person(): BelongsTo
+    public function persons(): BelongsToMany
     {
-        return $this->belongsTo(Person::class);
+        return $this->belongsToMany(Person::class)->using(EmailPerson::class);
     }
 }
