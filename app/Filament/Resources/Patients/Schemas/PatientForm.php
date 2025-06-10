31c952b95aff40205e7fc0fa7adfd9ceae607005
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-10 23:54:35 +0200

    Expand patient resource and remove obsolete resources

--- app/Filament/Resources/Patients/Schemas/PatientForm.php
+++ app/Filament/Resources/Patients/Schemas/PatientForm.php
@@ -2,7 +2,14 @@
 
 namespace App\Filament\Resources\Patients\Schemas;
 
+use App\Feature\Identity\Enums\ContactPointSystem;
+use App\Feature\Identity\Enums\Gender;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
 
 class PatientForm
@@ -11,9 +18,32 @@ class PatientForm
     {
         return $schema
             ->components([
-                Select::make('person_id')
-                    ->relationship('person', 'id')
-                    ->required(),
+                Group::make()
+                    ->relationship('person')
+                    ->schema([
+                        Textarea::make('first_name')
+                            ->required()
+                            ->columnSpanFull(),
+                        Textarea::make('last_name')
+                            ->required()
+                            ->columnSpanFull(),
+                        Textarea::make('pesel')
+                            ->columnSpanFull(),
+                        Textarea::make('id_number')
+                            ->columnSpanFull(),
+                        Select::make('gender')
+                            ->options(Gender::class),
+                        DatePicker::make('birth_date'),
+                        Repeater::make('contactPoints')
+                            ->relationship()
+                            ->schema([
+                                Select::make('system')
+                                    ->options(ContactPointSystem::class)
+                                    ->required(),
+                                TextInput::make('value')
+                                    ->required(),
+                            ]),
+                    ]),
             ]);
     }
 }
