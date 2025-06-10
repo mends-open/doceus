1cc8b9e927595d481691747666134b5e877b61bc
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-10 02:13:22 +0200

    Load and save patient contact info

--- app/Filament/Clusters/Managment/Resources/PatientResource/Pages/CreatePatient.php
+++ app/Filament/Clusters/Managment/Resources/PatientResource/Pages/CreatePatient.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Clusters\Managment\Resources\PatientResource\Pages;
 
 use App\Filament\Clusters\Managment\Resources\PatientResource;
+use App\Feature\Identity\Enums\ContactPointSystem;
+use App\Feature\Identity\Enums\ContactableType;
 use App\Models\Person;
 use Filament\Resources\Pages\CreateRecord;
 
@@ -14,7 +16,34 @@ class CreatePatient extends CreateRecord
     {
         $personData = $data['person'] ?? [];
 
+        $emails = collect($personData['emails'] ?? [])
+            ->pluck('value')
+            ->filter()
+            ->toArray();
+        $phones = collect($personData['phones'] ?? [])
+            ->pluck('value')
+            ->filter()
+            ->toArray();
+
+        unset($personData['emails'], $personData['phones']);
+
         $person = Person::create($personData);
+
+        foreach ($emails as $email) {
+            $person->contactPoints()->create([
+                'contactable_type' => ContactableType::Person,
+                'system' => ContactPointSystem::Email,
+                'value' => $email,
+            ]);
+        }
+
+        foreach ($phones as $phone) {
+            $person->contactPoints()->create([
+                'contactable_type' => ContactableType::Person,
+                'system' => ContactPointSystem::Phone,
+                'value' => $phone,
+            ]);
+        }
 
         $data['person_id'] = $person->id;
 
