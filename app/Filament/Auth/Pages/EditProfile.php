b705054cad3de5e2d5aa21a63152468394aaa5fe
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-11 00:14:41 +0200

    Fix Filament group import and extend profile form

--- app/Filament/Auth/Pages/EditProfile.php
+++ app/Filament/Auth/Pages/EditProfile.php
@@ -2,12 +2,14 @@
 
 namespace App\Filament\Auth\Pages;
 
+use App\Feature\Identity\Enums\Language;
 use Exception;
 use Filament\Auth\Pages\EditProfile as BaseEditProfile;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Component;
 use Filament\Schemas\Components\Group;
 use Filament\Schemas\Schema;
-use Filament\Schemas\Components\Component;
-use Filament\Forms\Components\TextInput;
 
 class EditProfile extends BaseEditProfile
 {
@@ -19,7 +21,11 @@ class EditProfile extends BaseEditProfile
         return $schema
             ->components([
                 $this->getEmailFormComponent(),
+                $this->getLanguageFormComponent(),
                 $this->getPersonFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getPasswordConfirmationFormComponent(),
+                $this->getCurrentPasswordFormComponent(),
             ]);
     }
 
@@ -28,7 +34,9 @@ class EditProfile extends BaseEditProfile
      */
     protected function getFirstNameFormComponent(): Component
     {
-        return TextInput::make('first_name');
+        return TextInput::make('first_name')
+            ->label(__('doceus.user.first_name'))
+            ->required();
     }
 
     /**
@@ -36,7 +44,9 @@ class EditProfile extends BaseEditProfile
      */
     protected function getLastNameFormComponent(): Component
     {
-        return TextInput::make('last_name');
+        return TextInput::make('last_name')
+            ->label(__('doceus.user.last_name'))
+            ->required();
     }
 
     /**
@@ -48,6 +58,16 @@ class EditProfile extends BaseEditProfile
             ->label(__('filament-panels::auth/pages/edit-profile.form.email.label'))
             ->email()
             ->required();
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function getLanguageFormComponent(): Component
+    {
+        return Select::make('language')
+            ->label(__('doceus.user.language'))
+            ->options(Language::class);
     }
 
 
