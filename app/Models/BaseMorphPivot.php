0471cf2a0fadfd350ad4d83a8597d725766f76de
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-12 00:43:39 +0200

    Remove contact point remnants and add tag base pivot

---
+++ app/Models/BaseMorphPivot.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use App\Feature\Revision\Interfaces\Revisionable;
+use App\Feature\Revision\Observers\RevisionableObserver;
+use App\Feature\Revision\Traits\LogsRevisions;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Relations\MorphPivot;
+
+#[ObservedBy([RevisionableObserver::class])]
+class BaseMorphPivot extends MorphPivot implements Revisionable
+{
+    use LogsRevisions;
+}
