d6edf781a58f7876dc4624e5c264c2903ee61be3
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-11 19:35:51 +0200

    Remove `ContactPointsRelationManager` and refactor patient contact handling:
    - Deleted `ContactPointsRelationManager` and its references from `PatientResource`.
    - Added contact management directly to `PatientForm` with `Repeater` fields for emails and phones.
    - Simplified `PatientForm` structure with updated schemas and sections.

--- app/Filament/Resources/Patients/Schemas/PatientForm.php
+++ app/Filament/Resources/Patients/Schemas/PatientForm.php
@@ -4,34 +4,50 @@ namespace App\Filament\Resources\Patients\Schemas;
 
 use App\Feature\Identity\Enums\Gender;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Repeater;
+use Filament\Schemas\Components\Flex;
 use Filament\Schemas\Components\Group;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class PatientForm
 {
+    /**
+     * @throws \Exception
+     */
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(1)
             ->components([
-                Group::make()
+                Flex::make([
+                    Section::make()
                     ->relationship('person')
                     ->schema([
                         TextInput::make('first_name')
-                            ->required()
-                            ->columnSpanFull(),
+                            ->required(),
                         TextInput::make('last_name')
-                            ->required()
-                            ->columnSpanFull(),
-                        TextInput::make('pesel')
-                            ->columnSpanFull(),
-                        TextInput::make('id_number')
-                            ->columnSpanFull(),
+                            ->required(),
+                        TextInput::make('pesel'),
+                        TextInput::make('id_number'),
                         Select::make('gender')
                             ->options(Gender::class),
                         DatePicker::make('birth_date'),
                     ]),
+                    Section::make()
+                    ->schema([
+                        Repeater::make('emails')
+                        ->simple(
+                            TextInput::make('value')
+                        ),
+                        Repeater::make('phones')
+                        ->simple(
+                            TextInput::make('value')
+                        ),
+                    ])
+                ])
             ]);
     }
 }
