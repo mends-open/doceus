2cd5b4aa1caca7878f261318082dce0aabde48ff
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-11 15:44:50 +0200

    Remove `ContactPoint` model and migration:
    - Deleted `ContactPoint` model and `contact_points` table migration, including related enums and relationships.
    - Updated `patients` table migration to include `telecom` JSON field as a replacement for contact points.

--- database/migrations/2025_06_09_175616_create_patients_table.php
+++ database/migrations/2025_06_09_175616_create_patients_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Person::class);
+            $table->jsonb('telecom');
             $table->softDeletes();
             $table->timestamps();
         });
