27ba79fa921cc50a45a4a714b1ceee7e8ba97a5a
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-12 22:40:16 +0200

    Simplify tag colors to Filament defaults

--- app/Feature/Tags/Enums/TagColor.php
+++ app/Feature/Tags/Enums/TagColor.php
@@ -11,28 +11,12 @@ enum TagColor: string
 {
     use HasTranslatableLabel;
 
-    case Slate = 'slate';
+    case Success = 'success';
+    case Warning = 'warning';
+    case Danger = 'danger';
+    case Info = 'info';
+    case Primary = 'primary';
     case Gray = 'gray';
-    case Zinc = 'zinc';
-    case Neutral = 'neutral';
-    case Stone = 'stone';
-    case Red = 'red';
-    case Orange = 'orange';
-    case Amber = 'amber';
-    case Yellow = 'yellow';
-    case Lime = 'lime';
-    case Green = 'green';
-    case Emerald = 'emerald';
-    case Teal = 'teal';
-    case Cyan = 'cyan';
-    case Sky = 'sky';
-    case Blue = 'blue';
-    case Indigo = 'indigo';
-    case Violet = 'violet';
-    case Purple = 'purple';
-    case Fuchsia = 'fuchsia';
-    case Pink = 'pink';
-    case Rose = 'rose';
 
     protected function translationPrefix(): string
     {
