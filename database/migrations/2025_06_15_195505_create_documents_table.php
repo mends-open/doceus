84eb9c47c75fdcc7e40d2f3c009d8ef50f7b42c7
dominik-mukrecki <dominik@mukrecki.pl>
2025-06-15 21:56:44 +0200

    Remove `Patient`, `Appointment`, and `Encounter` resources along with related components:
    - Deleted `PatientResource`, `AppointmentResource`, and `EncounterResource` with all related pages, schemas, and tables.
    - Removed corresponding models and refactored migrations.
    - Introduced `Observation` and `Document` models with migrations.

--- database/migrations/2025_06_15_195505_create_documents_table.php
+++ database/migrations/2025_06_15_195505_create_documents_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\Appointment;
-use App\Models\Practitioner;
+use App\Models\Encounter;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('encounters', function (Blueprint $table) {
+        Schema::create('documents', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Appointment::class);
-            $table->softDeletes();
+            $table->foreignIdFor(Encounter::class);
+            $table->softdeletes();
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('encounters');
+        Schema::dropIfExists('documents');
     }
 };
