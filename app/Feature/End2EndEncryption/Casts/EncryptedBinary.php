3265ada3bee96abb9a3e92491f7bfa0c2f018438
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-19 11:26:34 +0200

    Handle binary resources in EncryptedBinary cast

--- app/Feature/End2EndEncryption/Casts/EncryptedBinary.php
+++ app/Feature/End2EndEncryption/Casts/EncryptedBinary.php
@@ -9,9 +9,15 @@ class EncryptedBinary implements CastsAttributes
 {
     public function get($model, string $key, $value, array $attributes)
     {
-        return is_null($value)
-            ? null
-            : Crypt::decryptString(base64_encode($value));
+        if (is_null($value)) {
+            return null;
+        }
+
+        if (is_resource($value)) {
+            $value = stream_get_contents($value);
+        }
+
+        return Crypt::decryptString(base64_encode($value));
     }
 
     public function set($model, string $key, $value, array $attributes)
