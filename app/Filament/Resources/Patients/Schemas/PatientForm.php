d14e298e1c15d7b71d7fbe58bb4b9e8ca3779b24
dominik-mukrecki <64379788+dominik-mukrecki@users.noreply.github.com>
2025-06-23 11:45:03 +0200

    Switch wizards to tabs

--- app/Filament/Resources/Patients/Schemas/PatientForm.php
+++ app/Filament/Resources/Patients/Schemas/PatientForm.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Patients\Schemas;
 use App\Filament\Resources\People\Schemas\PersonForm;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 
 class PatientForm
@@ -13,15 +15,27 @@ class PatientForm
     {
         return $schema
             ->components([
-                Group::make()
-                    ->relationship('person')
-                    ->schema(PersonForm::configure(Schema::make())->getComponents()),
-                TextInput::make('email')
-                    ->email()
-                    ->required(),
-                TextInput::make('phone_number')
-                    ->tel()
-                    ->required(),
+                Tabs::make('Patient')
+                    ->key(null)
+                    ->tabs([
+                        Tab::make('Personal')
+                            ->key(null)
+                            ->schema([
+                                Group::make()
+                                    ->relationship('person')
+                                    ->schema(PersonForm::configure(Schema::make())->getComponents()),
+                            ]),
+                        Tab::make('Contact')
+                            ->key(null)
+                            ->schema([
+                                TextInput::make('email')
+                                    ->email()
+                                    ->required(),
+                                TextInput::make('phone_number')
+                                    ->tel()
+                                    ->required(),
+                            ]),
+                    ]),
             ]);
     }
 }
